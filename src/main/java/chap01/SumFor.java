ac588edba2689417c1e95f5b09a254584091f213
kho903 <gmldnr2222@naver.com>
2022-05-28 16:24:31 +0900

    feat: 1-5 SumFor

---
+++ src/main/java/chap01/SumFor.java
@@ -0,0 +1,18 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class SumFor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1 부터 n 까지의 합을 구합니다.");
+		System.out.print("n 의 값 : ");
+		int n = sc.nextInt();
+
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+	}
+}
