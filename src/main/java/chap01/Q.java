862e3aef5099f24a94ebd3b205aa11a5432cb98a
kho903 <gmldnr2222@naver.com>
2022-05-28 16:31:20 +0900

    feat: Q7 sumFor

--- src/main/java/chap01/Q.java
+++ src/main/java/chap01/Q.java
@@ -1,5 +1,7 @@
 package chap01;
 
+import java.util.Scanner;
+
 public class Q {
 	static int max4(int a, int b, int c, int d) {
 		return Math.max(a, Math.max(b, Math.max(c, d)));
@@ -29,10 +31,25 @@ public class Q {
 		else
 			return b;
 	}
+
+	static void sumFor(int n) {
+		System.out.print("n 의 값 : ");
+		System.out.println(n);
+		int sum = 1;
+		System.out.print(1);
+		for (int i = 2; i <= n; i++) {
+			System.out.print(" + " + i);
+			sum += i;
+		}
+		System.out.println(" = " + sum);
+		System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+	}
+
 	public static void main(String[] args) {
 		System.out.println(max4(1, 3, 4, 2));
 		System.out.println(min3(1, 3, 4));
 		System.out.println(min4(1, 3, 4, 2));
 		System.out.println(med3(1, 3, 2));
+		sumFor(7);
 	}
 }
