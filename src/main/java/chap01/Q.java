ed1fc9fc2b655bc9d1fecfea60489a10076537fe
kho903 <gmldnr2222@naver.com>
2022-05-28 16:16:53 +0900

    feat: Q4 med3

--- src/main/java/chap01/Q.java
+++ src/main/java/chap01/Q.java
@@ -13,9 +13,26 @@ public class Q {
 		return Math.min(a, Math.min(b, Math.min(c, d)));
 	}
 
+
+	static int med3(int a, int b, int c) {
+		if (a >= b)
+			if (b >= c)
+				return b;
+			else if (a <= c)
+				return a;
+			else
+				return c;
+		else if (a > c)
+			return a;
+		else if (b > c)
+			return c;
+		else
+			return b;
+	}
 	public static void main(String[] args) {
 		System.out.println(max4(1, 3, 4, 2));
 		System.out.println(min3(1, 3, 4));
 		System.out.println(min4(1, 3, 4, 2));
+		System.out.println(med3(1, 3, 2));
 	}
 }
