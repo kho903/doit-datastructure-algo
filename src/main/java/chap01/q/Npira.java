cb770899a856fc5ae3b6f85179c0247f37cbe8a9
kho903 <gmldnr2222@naver.com>
2022-05-28 17:42:36 +0900

    feat: Q16, 17 spria, npira

---
+++ src/main/java/chap01/q/Npira.java
@@ -0,0 +1,26 @@
+package chap01.q;
+
+public class Npira {
+	static void spira(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j < 2 * n; j++) {
+				if (j > n - i && j < n + i) {
+					System.out.print(i);
+				} else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		spira(4);
+	}
+}
+
+/*
+1 : 4
+2 : 3, 4, 5
+3 : 2, 3, 4, 5, 6
+4 : 1, 2, 3, 4, 5, 6, 7
+ */
