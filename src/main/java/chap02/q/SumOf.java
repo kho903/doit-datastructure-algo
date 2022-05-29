1f474708303743c01432e8b2341c976db92ef6b6
kho903 <gmldnr2222@naver.com>
2022-05-29 15:03:45 +0900

    feat: Q2-3 sumOf

---
+++ src/main/java/chap02/q/SumOf.java
@@ -0,0 +1,15 @@
+package chap02.q;
+
+public class SumOf {
+	static int sumOf(int[] a) {
+		int sum = 0;
+		for (int i = 0; i < a.length; i++)
+			sum += a[i];
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		System.out.println(sumOf(a));
+	}
+}
