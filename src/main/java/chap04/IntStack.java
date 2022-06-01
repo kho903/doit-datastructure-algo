caeb3ceecb4298e4c061a7cba27e7c227d4f0545
kho903 <gmldnr2222@naver.com>
2022-06-01 15:31:53 +0900

    feat: 4-1[A] IntStack

---
+++ src/main/java/chap04/IntStack.java
@@ -0,0 +1,26 @@
+package chap04;
+
+public class IntStack {
+	private int max;
+	private int ptr;
+	private int[] stk;
+
+	public class EmptyIntStackException extends RuntimeException {
+		public EmptyIntStackException() {}
+	}
+
+	public class OverflowIntStackException extends RuntimeException {
+		public OverflowIntStackException() {}
+	}
+
+	public IntStack(int capacity) {
+		ptr = 0;
+		max = capacity;
+
+		try {
+			stk = new int[max];
+		} catch (OutOfMemoryError error) {
+			max = 0;
+		}
+	}
+}
