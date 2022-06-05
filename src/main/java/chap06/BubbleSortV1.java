9feb622470a360eb8f9d9207a5fc195e8c9cde11
kho903 <gmldnr2222@naver.com>
2022-06-05 18:14:26 +0900

    feat: 6-1 BubbleSortV1

---
+++ src/main/java/chap06/BubbleSortV1.java
@@ -0,0 +1,29 @@
+package chap06;
+
+import java.util.Arrays;
+
+public class BubbleSortV1 {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+
+	static void bubbleSort(int[] a, int n) {
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = n - 1; j > i; j--)
+				if (a[j - 1] > a[j])
+					swap(a, j - 1, j);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Bubble Sort V1");
+		int[] a = {3, 2, 5, 1, 9, 7, 8};
+		bubbleSort(a, a.length);
+		System.out.println(Arrays.toString(a));
+		a = new int[] {5, 2, 3, 1, 9, 6};
+		bubbleSort(a, a.length);
+		System.out.println(Arrays.toString(a));
+	}
+}
