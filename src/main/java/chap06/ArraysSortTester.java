af8e941d38fc3cdea40a8c7e806c9f47d821d4b0
kho903 <gmldnr2222@naver.com>
2022-06-08 12:45:10 +0900

    feat: 6-13 ArraysSortTester

---
+++ src/main/java/chap06/ArraysSortTester.java
@@ -0,0 +1,12 @@
+package chap06;
+
+import java.util.Arrays;
+
+public class ArraysSortTester {
+	public static void main(String[] args) {
+		int[] x = {22, 5, 11, 32, 120, 68, 70};
+
+		Arrays.sort(x);
+		System.out.println(Arrays.toString(x));
+	}
+}
